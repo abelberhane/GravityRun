6a435b0a987dc25fee921b4ac1ae0ac39fcfe05a
Abel <abelberhane@gmail.com>
2017-07-29 07:36:12 -0400

    Side scrolling Batman game where he defys physics itself!

---
+++ gravityRun/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace gravityRun
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            this.p3 = new System.Windows.Forms.PictureBox();
+            this.p4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score:";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::gravityRun.Properties.Resources.batman_1;
+            this.player.Location = new System.Drawing.Point(119, 128);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(45, 48);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.player.TabIndex = 1;
+            this.player.TabStop = false;
+            // 
+            // p1
+            // 
+            this.p1.BackColor = System.Drawing.Color.Transparent;
+            this.p1.Image = global::gravityRun.Properties.Resources.platform;
+            this.p1.Location = new System.Drawing.Point(104, 235);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(273, 28);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1.TabIndex = 2;
+            this.p1.TabStop = false;
+            this.p1.Tag = "Platform";
+            // 
+            // p2
+            // 
+            this.p2.BackColor = System.Drawing.Color.Transparent;
+            this.p2.Image = global::gravityRun.Properties.Resources.platform;
+            this.p2.Location = new System.Drawing.Point(491, 235);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(273, 28);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2.TabIndex = 3;
+            this.p2.TabStop = false;
+            this.p2.Tag = "Platform";
+            // 
+            // p3
+            // 
+            this.p3.BackColor = System.Drawing.Color.Transparent;
+            this.p3.Image = global::gravityRun.Properties.Resources.platform;
+            this.p3.Location = new System.Drawing.Point(304, 26);
+            this.p3.Name = "p3";
+            this.p3.Size = new System.Drawing.Size(273, 28);
+            this.p3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p3.TabIndex = 4;
+            this.p3.TabStop = false;
+            this.p3.Tag = "Platform";
+            // 
+            // p4
+            // 
+            this.p4.BackColor = System.Drawing.Color.Transparent;
+            this.p4.Image = global::gravityRun.Properties.Resources.platform;
+            this.p4.Location = new System.Drawing.Point(705, 26);
+            this.p4.Name = "p4";
+            this.p4.Size = new System.Drawing.Size(273, 28);
+            this.p4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p4.TabIndex = 5;
+            this.p4.TabStop = false;
+            this.p4.Tag = "Platform";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 261);
+            this.Controls.Add(this.p4);
+            this.Controls.Add(this.p3);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Batman Gravity Run";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+        private System.Windows.Forms.PictureBox p3;
+        private System.Windows.Forms.PictureBox p4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ gravityRun/Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gravityRun
+{
+    public partial class Form1 : Form
+    {
+        //Global Variables
+        bool landed = false;
+        int gravity = 5;
+        int score = 0;
+        int platformSpeed = 10;
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Check to see what key has been pushed.
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            //if the Spacebar is pressed and we have landed, disable gravity and allow the player to jump!
+            if (e.KeyCode == Keys.Space && landed == true)
+            {
+                gravity = -gravity;
+                landed = false;
+            }
+
+            //If R is pressed, execute the Reset function and bring the game back to its start.
+            if (e.KeyCode == Keys.R)
+            {
+                reset();
+            }
+        }
+
+        //Logic for the timer, gravity and how points are incremented
+        private void timer1_Tick(object sender, EventArgs e)
+        {   
+            //As the timer progresses, gravity brings the player downward. Player will always be 80 pixels away from the border.
+            player.Top += gravity;
+            player.Left = 80;
+            label1.Text = "Score: " + score;
+
+            //Logic for Controls and moving the platform to the left. 
+            foreach (Control x in this.Controls)
+            {
+                if ( x is PictureBox && x.Tag == "Platform")
+                {
+                    x.Left -= platformSpeed;
+                
+                    if (x.Left < -500)
+                    {
+                        //Difficulty is controlled here, the rnd.Next shows how we change the width of x randomly to make it challenging.
+                        x.Width = rnd.Next(100, 300);
+                        x.Left = 500;
+                        score++;
+                    }
+                }
+            }
+
+            // Logic for the which image is shown. If the player has landed on P1 or P2, show batman_1
+            if (player.Bounds.IntersectsWith(p2.Bounds) || player.Bounds.IntersectsWith(p1.Bounds))
+            {
+                landed = true;
+                player.Top = p2.Top - player.Height;
+                player.Image = Properties.Resources.batman_1;
+            }
+
+            // Logic for the which image is shown. If the player has landed on P3 or P4, show batman_2
+            if (player.Bounds.IntersectsWith(p3.Bounds) || player.Bounds.IntersectsWith(p4.Bounds))
+            {
+                landed = true;
+                player.Top = p3.Top + p3.Height;
+                player.Image = Properties.Resources.batman_2;
+            }
+
+            //Logic for how the game ends. If the player exceeds the size of the game, stop the timer and change the text to Post Game Reset
+            if(player.Top < -40 || player.Top > ClientSize.Height)
+            {
+                timer1.Stop();
+                label1.Text += " -- Press R to reset";
+            }
+        }
+
+        //The Settings the game will be set to when it restarts. 
+        //......1) Bring the score to 0
+        //......2) Bring the Width of the 4 platforms back to the default size
+        //......3) Bring the Player back to the starting X and Y position
+        //......4) Bring the Platforms back to the starting X and Y positions
+        //......5) Restart the timer
+        private void reset()
+        {
+            score = 0;
+            p1.Width = 273;
+            p2.Width = 273;
+            p3.Width = 273;
+            p4.Width = 273;
+            player.Left = 119;
+            player.Top = 128;
+
+            p1.Left = 104;
+            p1.Top = 235;
+
+            p2.Left = 491;
+            p2.Top = 235;
+
+            p3.Left = 304;
+            p3.Top = 26;
+
+            p4.Left = 705;
+            p4.Top = 26;
+
+            timer1.Start();
+        }
+    }
+}
